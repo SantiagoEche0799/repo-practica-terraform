b0eff082d66688776a493e5908cd199f6d2bb191
SantiagoEche0799 <santiago.eche.duque.99910@gmail.com>
2025-10-22 16:47:22 -0500

    se crea la carpeta primer-recurso-en-terraform y se mueven los archivos main.tf y readme.md. Adicionalmente se actualiza el .gitignore, para omitir los archivos de configuración de terraform.

--- primer-recurso-en-terraform/main.tf
+++ primer-recurso-en-terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 3.0"
+        version = "~> 5.0"
     }
   }
 }
@@ -12,6 +12,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+  ami = "ami-0bbdd8c17ed981ef9" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t3.micro"
 }
